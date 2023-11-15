b3e9b17978a3fbdeb1d4182108d1f6a48a23e237
Georgia Wright <georgiawright617@gmail.com>
2023-11-15 17:35:44 +0000

    pandas activity

--- cereal.py
+++ cereal.py
@@ -30,13 +30,34 @@ for i in df.columns:
 
 protein_df = df.loc[:, "name":"protein" : 4]
 
-for i in protein_df:
-    if protein_df[protein_df.select_dtypes (include= "int") > 4]:
-        print(i)
-    else:
-        print()
+# for i in protein_df:
+#     if protein_df[protein_df.select_dtypes (include= "int") > 4]:
+#         print(i)
+#     else:
+#         print()
 
         
+# the query method works best for finding figures of a certain condition
+
+
+print(df.query('protein > 4')["name"],["protein"])
+
+
+
+
+
+
+# bran flakes:
+
+# print(df)
+
+print(df[df["name"]== "Bran Flakes"].index.values) #this finds the index number of the row where name is Bran Flakes
+
+#index number of Bran flakes is 9.
+
+print(df.iloc[9]) #this prints the row at index position of 9
+
+
 
 
 
