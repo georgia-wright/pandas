d39f4148b7dabb12f9f97170cdf658defd0e0ca1
Georgia Wright <georgiawright617@gmail.com>
2023-11-27 10:04:11 +0000

    cleaning

--- cleaning.py
+++ cleaning.py
@@ -58,7 +58,119 @@ df["Time"] = pd.to_datetime(df["Time"])
 df["Time"] = df["Time"].apply(extract_time)
 
 
+# print(df)
+
+
+
+#average price of an item
+# sum of items
+#sum of amount
+
+#sum of amounts divided by sum of items
+
+
+sum_items = df["Items"].sum()
+
+
+
+sum_items = int(sum_items)
+
+print(sum_items)
+
+
+sum_amount = df["Amount"].sum()
+
+
+
+sum_amount = int(sum_amount)
+
+
+print(sum_amount)
+
+
+avg_price_item = (sum_amount/sum_items)
+
+
+print(avg_price_item)
+
+#(avg price of item is 3.3709.....)
+
+
+
+
+#average basket price
+
+#sum of amounts divided by number of transactions
+
+
+# count of transactions =
+
 print(df)
 
+print(df.shape)
+
+# 30 rows, 5 columns
+
+# 30 transactions
+
+print(sum_amount/30)
+
+# average basket price is 6.966666...
 
 
+
+
+#maximum spend in one transaction
+
+#.max of amounts
+
+
+# print(df.max(axis = 1))
+
+#make a new column of just amount
+
+df2 = df["Amount"]
+
+print(df2)
+
+print(df2.max())
+
+#max amount is 56.5
+
+
+
+
+#minimum spend
+
+#.min of df2
+
+print(df2.min())
+
+#min spend is 2.20
+
+
+
+
+#most common spend amount
+
+#the mode of amounts
+
+
+print(df2.mode().loc[0])
+
+#mode is 2.85
+
+
+
+
+#most common payment type
+
+# mode of currency type column
+
+df3 = df["Currency"]
+
+print(df3)
+
+print(df3.mode())
+
+#most common currency / payment type is Debit
